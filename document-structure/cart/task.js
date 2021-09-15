a8c494eceb3820d04303ef27ac06e810da25ae32
unknown <kgeluza@mail.ru>
2021-09-15 20:54:51 +0400

    Готово

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -36,13 +36,14 @@ for(let btn of addBasket) {
        const dataId = products[btnIndex].dataset.id;
        const imageProd = image[btnIndex].src;
        const carts = Array.from(basket.children);
-
-       if(!carts.find(item => item.dataset.id === dataId)) {
+       
+       let findItem = carts.findIndex(item => item.dataset.id === dataId);
+       if(findItem === -1) {
          basket.insertAdjacentHTML("afterBegin", `<div class="cart__product" data-id="${dataId}">
          <img class="cart__product-image" src="${imageProd}">
          <div class="cart__product-count"> ${count}</div>
      </div>`)
-       } else {
+       } else if(findItem > -1) {
         const prodBasket = carts.find((item) => item.dataset.id === dataId);
         let prodCount = prodBasket.querySelector(".cart__product-count");
         prodCount.textContent = Number(prodCount.textContent) + Number(count);
