ec2a1c3c3b7b453b5905afc3c8eba8fade81af5b
unknown <kgeluza@mail.ru>
2021-09-16 19:52:24 +0400

    Исправления в задаче2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,9 +1,45 @@
 const input = document.getElementById("task__input");
 const list = document.getElementById("tasks__list");
 let forClear = document.getElementsByClassName("task__remove");
+const addBtn = document.getElementById("tasks__add");
 
+function addTasks() {
+  list.innerHTML += `<div class="task">
+  <div class="task__title">
+    ${input.value}
+  </div>
+  <a href="#" class="task__remove">&times;</a>
+</div>`;
+  }
+
+addBtn.addEventListener("click", event => {
+   event.preventDefault() 
+   if(input.value.trim() !== "") { 
+   addTasks();
+   }
+})
+
+document.addEventListener("keypress", event => {
+   if(event.key === "Enter" && input.value.trim() !== "") {
+    event.preventDefault()
+    addTasks()
+  }
+ 
+})
+
+
+list.addEventListener("click", (event) => {
+  let target = event.target;
+  if (target.classList.contains('task__remove')) {
+      target.parentElement.remove();
+  } 
+ }); 
+
+
+
+/*
 document.addEventListener("keypress", (event) => {
-  if(event.key === "Enter" && input.value !== "")  {
+  if(event.key === "Enter" && input.value.trim() !== "")  {
   list.innerHTML += `<div class="task">
   <div class="task__title">
     ${input.value}
@@ -16,22 +52,12 @@ document.addEventListener("keypress", (event) => {
 
 list.addEventListener("click", (event) => {
   let target = event.target;
-
   if (target.classList.contains('task__remove')) {
       target.parentElement.remove();
-  }
-  
- });    
+  } 
+ });   */
 
 
 
 
 
-/*
-forClear.forEach(element => 
-  element.onclick = function() {
-    element.closest(".task").remove;
-  }
-  )
-
-*/
